2bfb90aa12152e7acfbd2168caa78373ae1ac5d3
MainamSuwan48 <112855044+SexyCornman@users.noreply.github.com>
2024-02-15 18:08:57 +0700

    fixed Syntax error that prevent from getting user by id

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -4,22 +4,26 @@ const jwtService = require("../services/jwt-service");
 const userService = require("../services/user-service");
 
 const authenticate = catchError(async (req, res, next) => {
-    const authorization = req.headers.authorization;
-    console.log("**************",req.headers.authorization)
-    if (!authorization || !authorization.startsWith('Bearer ')) {
-      createError('invalid authorization header', 401);
-    }
-    const token = authorization.split(' ')[1];
-    const decodedPayload = jwtService.verify(token);
-  
-    const user = await userService.findUserById(decodedPayload.userId);
-    if (!user) {
-      createError('user was not found', 401);
-    }
-    delete user.password;
-    req.user = user;
-    next();
-  });
-  
-  module.exports = authenticate;
-  
+  const authorization = req.headers.authorization;
+  console.log("**************", authorization);
+  if (
+    !authorization
+    ||     !authorization.startsWith("Bearer ")
+  ) {
+    createError("invalid authorization header", 401);
+  }
+ 
+  const token = authorization.split(' ')[1];
+  const decodedPayload = jwtService.verifyToken(token);
+  console.log(decodedPayload)
+
+  const user = await userService.findUserById(decodedPayload.id);
+  if (!user) {
+    createError("user was not found", 401);
+  }
+  delete user.password;
+  req.user = user;
+  next();
+});
+
+module.exports = authenticate;
