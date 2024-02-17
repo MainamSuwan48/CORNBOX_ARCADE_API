ddaf2ceb660ed5dcf4a92be996bd50bd0ceca180
MainamSuwan48 <112855044+SexyCornman@users.noreply.github.com>
2024-02-17 21:55:16 +0700

    finished api for cart management and i will go watch some anime this is too much

--- src/controller/product-controller.js
+++ src/controller/product-controller.js
@@ -19,4 +19,50 @@ exports.getProductById = catchError(async (req, res) => {
   res.status(200).json(product);
 });
 
+exports.addToCartOrCreateCart = catchError(async (req, res) => {
+    const { userId, productItemId, quantity, attribute } = req.body;
+  let cart = await userHasCart(userId);
 
+  if (!cart) {
+    // Create a new cart for the user
+    cart = await prisma.shoppingCart.create({
+      data: {
+        userId: userId,
+      },
+    });
+  }
+
+  // Add the item to the user's cart
+  const newItem = await addItemToCart(
+    cart.id,
+    productItemId,
+    quantity,
+    attribute
+  );
+
+  return newItem;
+});
+
+exports.getCartItems = catchError(async (req, res) => {
+  const { userId } = req.body;
+  const cartItems = await productService.getCartItems(userId);
+  res.status(200).json(cartItems);
+});
+
+exports.findCartItem = catchError(async (req, res) => {
+  const { cartItemId } = req.body;
+  const item = await productService.findCartItem(cartItemId);
+  res.status(200).json(item);
+});
+
+exports.updateCartItem = catchError(async (req, res) => {
+  const { cartId, cartItemId, newQuantity} = req.body;
+  const updatedItem = await productService.updateCartItem(cartId, cartItemId, newQuantity);
+  res.status(200).json(updatedItem);
+});
+
+exports.deleteCartItem = catchError(async (req, res) => {
+  const { cartItemId } = req.body;
+  const deletedItem = await productService.deleteCartItem(cartItemId);
+  res.status(200).json(deletedItem);
+});
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -6,5 +6,10 @@ router = express.Router();
 
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
+router.post("/cart", productController.addToCartOrCreateCart);
+router.get("/cart", productController.getCartItems);
+router.get("/cart/", productController.findCartItem);
+router.put("/cart", productController.updateCartItem);
+router.delete("/cart", productController.deleteCartItem);
 
 module.exports = router;
